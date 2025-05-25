fd872181d90f6351673704fca49e8465667e9875
codefythecode <135412064+codefythecode@users.noreply.github.com>
2025-05-25 11:39:45 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,5 +1,22 @@
-function myFunction() {
-    var a=4;
-    document.getElementById("demo").innerHTML=a*a;
+var dice;
 
+var dices = ['&#9856;', '&#9857;', '&#9858;', '&#9859;', '&#9860;', '&#9861;'];
+var stoped = true;
+var t;
+function change(){
+    var random = Math.floor(Math.random() * 6);
+    dice.innerHTML = dices[random];
 }
+function stopstart(){
+    if(stoped){
+        stoped = false;
+        t = setInterval(change, 100);
+    }else{
+        stoped = true;
+        clearInterval(t);
+    }
+}
+window.onload = function(){
+    dice= document.getElementById('dice');
+    stopstart();
+};
